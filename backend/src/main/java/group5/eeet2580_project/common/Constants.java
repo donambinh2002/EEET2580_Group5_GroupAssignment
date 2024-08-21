4dfc131e5cc5107750eceaa0eb7d2979ca878735
Duc Le <duc.le4@onemount.com>
2024-08-21 14:12:45 +0700

    backend: service refactor

---
+++ backend/src/main/java/group5/eeet2580_project/common/Constants.java
@@ -0,0 +1,9 @@
+package group5.eeet2580_project.common;
+
+public class Constants {
+    public interface ROLE_KEYS {
+        String USER = "USER";
+        String STAFF = "STAFF";
+        String ROLE_REGEX = "^(USER|STAFF)$";
+    }
+}
