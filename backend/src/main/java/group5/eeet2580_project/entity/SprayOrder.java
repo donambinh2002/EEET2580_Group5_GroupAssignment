074d445bbc0165ce0f0d381cbf093ee04998fcae
Duc Le <dealoux@gmail.com>
2024-09-15 20:25:56 +0700

    backend: order fix

--- backend/src/main/java/group5/eeet2580_project/entity/SprayOrder.java
+++ backend/src/main/java/group5/eeet2580_project/entity/SprayOrder.java
@@ -4,8 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "spray_orders")
@@ -15,31 +14,45 @@ import java.time.LocalTime;
 @Setter
 @Builder
 public class SprayOrder implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-
-    private String cropType;
-    private double farmLandArea;
-    private LocalDate desiredDate;
-    private LocalTime desiredTime;
-    private double totalCost;
-    private Integer rating;
-    private String feedbackText;
-    private Integer feedbackRating;
-
     public enum Status {
         PENDING, CANCELLED, CONFIRMED, ASSIGNED, IN_PROGRESS, COMPLETED
     }
 
+    public enum CropType{
+        FRUIT, CEREAL, VEGETABLE
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private float farmLandArea;
+
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CropType cropType;
+
+    @ManyToOne
+    @JoinColumn(name = "farmer_id", nullable = false)
+    private User farmer;
+
+    @OneToOne
+    @JoinColumn(name = "session_id")
+    private SpraySession spraySession;
+
+    private LocalDateTime orderTime;
+    private LocalDateTime desiredStartTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Status status;
 
-    public double getFarmlandArea() {
+    public double getTotalCost() {
+        return this.getFarmLandArea() * 30000;
+    }
+
+    public double getFarmlandAreaInMetres() {
         return Math.round((this.farmLandArea / 1000.0) * 10.0) / 10.0;
     }
 }
